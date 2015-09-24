872425249d53ec9683a67dfe90ed002adb544e47
Qifan Lu <lqf.1996121@gmail.com>
2015-09-24 06:55:17 +0000

    Add 'models.py' stub from django model discovery

---
+++ main/models.py
@@ -0,0 +1,3 @@
+# (This is only a stub for import packages & modules that contains needed files)
+from UserModels import User,EmailValidationRecord
+from DZModels import DiscussionZoneBoard,DiscussionZonePost,DiscussionZoneContent
